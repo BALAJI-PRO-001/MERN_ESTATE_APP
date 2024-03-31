a8686be3b441b76eae3bee93d370f50198bc8e3e
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-03-31 14:30:59 +0530

    No commit message

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  console.log("data");
+  return (
+    <div>
+      <h1 className="text-red-500">Welcome To React</h1>
+    </div>
+  )
+}
