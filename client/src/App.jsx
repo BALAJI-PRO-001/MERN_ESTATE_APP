7ab02029119a8856f4d6cd2ac942015645ccae63
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-03-31 16:08:51 +0530

    Feat: created server side code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignOut from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  console.log("data");
   return (
-    <div>
-      <h1 className="text-red-500">Welcome To React</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
