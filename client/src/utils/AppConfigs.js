890289a0d7ac091f6cae6ad0a6af732707b9716e
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-04-21 13:18:09 +0530

    Mod: modified

--- client/src/utils/AppConfigs.js
+++ client/src/utils/AppConfigs.js
@@ -43,7 +43,31 @@ const signinConfig = [
 ];
 
 
+const profileConfig = [
+  {
+    id: "userName",
+    placeholder: "Username . . . .",
+    type: "text",
+    imgSrc: ["/images/icons/user-name-icon.png"],
+  },
+  {
+    id: "email",
+    placeholder: "Email . . . .",
+    type: "text",
+    imgSrc: ["/images/icons/email-icon.png"],
+  },
+  {
+    id: "password",
+    placeholder: "Password . . . .",
+    type: "password",
+    imgSrc: ["/images/icons/lock-icon.png", "/images/icons/eye-close-icon.png"],
+  },
+];
+
+
+
 export {
   signupConfig, 
-  signinConfig
+  signinConfig,
+  profileConfig
 };
