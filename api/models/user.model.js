66ca864aee5fff67c364d262139cd0b6985e4c7d
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-04-21 11:10:39 +0530

    Mod: user model is changed with avatar

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
+  }, 
+  avatar: {
+    type: String,
+    default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw0draaBzErkbX06xp9006wW&ust=1713762489892000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJi21fXE0oUDFQAAAAAdAAAAABAE",
   }
 }, {timestamps: true});
 
