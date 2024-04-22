f4e753be1663397435cb8b3b5025d0ed082da9f2
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-04-22 06:35:54 +0530

    Chor: input border color changed

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -116,7 +116,7 @@ export default function Signin() {
                 type={config.type}
                 id={config.id}
                 placeholder={config.placeholder}
-                className="border p-3 rounded-lg pl-10 focus:border-blue-600 focus:outline-none w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
+                className="border p-3 rounded-lg pl-10 focus:border-green-600 focus:outline-none w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
                 onChange={onInputHandler}
                 onBlur={onBlurHandler}
               />
