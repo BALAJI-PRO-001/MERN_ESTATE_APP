4d98e85603a6997e967baf86a346432d132c2ae8
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-05-12 17:40:02 +0530

    Feat: Complete profile page functionality added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { profileConfig } from "../utils/AppConfigs.js";
 import UserInterface from "../utils/UserInterface.js";
 import Validator from "../utils/Validator.js";
@@ -6,20 +6,38 @@ import CommonFunction from "../utils/CommonFunctions.js";
 import { useRef, useState, useEffect } from "react";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { app } from "../utils/firebase.js";
+import { 
+  updateUserStart, 
+  updateUserSuccess, 
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure
+} from "../redux/user/userSlice.js";
+
 
 const ui = new UserInterface();
 const commonFunction = new CommonFunction();
 const validator = new Validator();
 
 
+
 export default function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [progress, setProgress] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
- 
+  const dispatch = useDispatch();
+  const [boolean, setBoolean] = useState(true);
+  const [message, setMessage] = useState("");
+  const [errMessage, setErrMessage] = useState("");
+  const [imgUploadMessage, setImgUploadMessage] = useState("");
+
 
   function setIcon(event) {
     ui.setIcon(
@@ -35,35 +53,105 @@ export default function Profile() {
   }
 
 
-  function onBlurHandler(event) {
-    const inputElement = event.target;
-    if (inputElement.id === "userName") 
-      validator.isvalidName(inputElement);
+  function onInputHandler(event) {
+    if (event.target.id === "userName") 
+    setBoolean(validator.isvalidName(event.target));
 
-    if (inputElement.id === "email")
-      validator.isvalidEmail(inputElement);
+    if (event.target.id === "email")
+    setBoolean(validator.isvalidEmail(event.target));
 
-    if (inputElement.id === "password") 
-      validator.isvalidPassword(inputElement);
+    if (event.target.id === "password") 
+    setBoolean(validator.isvalidPassword(event.target));
+
   }
 
 
   async function handleSubmit(event) {
     event.preventDefault();
-    const inputElements = commonFunction.getSiblings(event.target, "input");
-    const booleans = [];
+    const emailInput = event.target.parentElement.querySelector("#email");
 
-    for (let inputElement of inputElements) {
-      if (inputElement.id === "userName") 
-        booleans.push(validator.isvalidName(inputElement));
+  
+    if (boolean && !fileUploadError) {
+      try {
+        dispatch(updateUserStart());
+        const res = await fetch(`/api/user/update/${currentUser._id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(formData)
+        });
 
-      if (inputElement.id === "email")
-        booleans.push( validator.isvalidEmail(inputElement));
+        const data = await res.json();
+        if (data.statusCode === 409) {
+          dispatch(updateUserFailure());
+          const errorElement = commonFunction.getSibling(emailInput, "p");
+          ui.setBorder(emailInput, ui.BORDER_1PX_RED);
+          ui.setMessage(errorElement, "Email address is already in use. Please try another email . . . .");
+          return;
+        }
 
-      if (inputElement.id === "password")
-        booleans.push(validator.isvalidPassword(inputElement));
+        if (data.statusCode === 401) {
+          setErrMessage(data.message);
+          dispatch(updateUserFailure());
+          return;
+        }
+
+        dispatch(updateUserSuccess(data));
+        setMessage("Profile updated successfully")
+        setTimeout(() => {
+          setMessage("");
+        }, 2000);
+
+      } catch(error) {
+        setErrMessage(error.message);
+      }
+    } 
+
+  }
+
+
+  async function handleDeleteUser() {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setErrMessage(data.message);
+        dispatch(deleteUserFailure());
+        return;
+      }
+
+      dispatch(deleteUserSuccess());
+    } catch (error) {
+      setErrMessage(error.message);
     }
-     
+
+  }
+
+
+  async function handleSignOut() {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signOut",{
+        method: "GET"
+      });
+  
+      const data = await res.json();
+      if (data.success === false) {
+        setErrMessage(data.message);
+        dispatch(signOutUserFailure());
+        return;
+      }
+  
+      dispatch(signOutUserSuccess());
+    } catch (error) {
+      dispatch(signOutUserFailure());
+      setErrMessage(error.message);
+    }
   }
 
 
@@ -96,17 +184,22 @@ export default function Profile() {
               return {...formData, avatar: downloadURL}
             });
           });
+        setImgUploadMessage("Image successfully uploaded!");
+        setTimeout(() => {
+          setImgUploadMessage("");
+        }, 2000);
       }
     );
   }
 
 
-  function onInputHandler(event) {
+  function onChangeHandler(event) {
     setFormData({
       ...formData, 
-      [event.target.id]: event.target.value,
+      [event.target.id]: event.target.value.trim(),
     });
   } 
+
   
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -118,18 +211,14 @@ export default function Profile() {
           alt="profile"
           onClick={() => fileRef.current.click()}
         />
-        <img 
-          className="h-14 w-14 absolute self-center top-10 ml-2"
-          src="/images/icons/image-upload-icon.png"
-          alt="icon"
-          hidden
-        />
-        <p className="text-sm self-center">
+        <p className="text-sm self-center mt-1">
           {
             fileUploadError ? <span className="text-red-600 font-semibold">ERROR: The image must be less than 2MB!</span> :
             progress > 0 && progress < 100 ? <span className="text-slate-700 font-semibold">{`Uploading: ${progress}%`}</span> :
-            progress === 100 ? <span className="text-green-600 font-semibold">Image successfully uploaded!</span> : ""
+            progress === 100 ? <span className="text-green-600 font-semibold">{imgUploadMessage}</span> : ""
           }
+          {errMessage && <span className="text-red-600 font-semibold text-1xl">{`ERROR: ${errMessage} (Access Denied)`}</span>}
+          {message && <span className="text-green-600 font-semibold text-1xl">{message}</span>}
         </p>
         {profileConfig.map((config) => {
           return (
@@ -139,8 +228,12 @@ export default function Profile() {
                 id={config.id}
                 placeholder={config.placeholder}
                 className="border p-3 rounded-lg pl-10 focus:border-green-600 focus:outline-none w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
-                onChange={onInputHandler}
-                onBlur={onBlurHandler}
+                onChange={onChangeHandler}
+                onInput={onInputHandler}
+                defaultValue={
+                  config.id == "userName" ? currentUser.userName : "" ||
+                  config.id == "email" ? currentUser.email : ""
+                }
               />
               <img
                 src={config.imgSrc[0]}
@@ -163,33 +256,36 @@ export default function Profile() {
           );
         })}
         <button
-          className="bg-slate-700 text-white p-3 mt-3 rounded-lg hover:opacity-95 disabled:opacity-80"
-          style={{ fontFamily: "sans-serif" }}
+          className="bg-slate-700 text-white p-3 mt-3 rounded-lg hover:opacity-95 disabled:opacity-80  tracking-wider font-semibold"
           onClick={handleSubmit}
-          // disabled={loading}
+          disabled={loading && boolean}
         >
-          UPDATE
+          {loading ? "LOADING . . . " : "UPDATE"}
         </button>
       </form>
       <div className="flex justify-between gap-2 mt-2">
-        <span className="font-semibold p-3 bg-red-600 rounded-lg text-white cursor-pointe w-1/2 text-center hover:opacity-90">
-          Delete Account
+        <span 
+          className="font-semibold p-3 bg-red-600 rounded-lg text-white cursor-pointe w-1/2 text-center hover:opacity-90 tracking-wider"
+          onClick={handleDeleteUser}
+          >
+            Delete Account
         </span>
-        <span className="font-semibold p-3 bg-red-600 rounded-lg text-white cursor-pointer w-1/2 text-center hover:opacity-90">
+        <span 
+          className="font-semibold p-3 bg-red-600 rounded-lg text-white cursor-pointer w-1/2 text-center hover:opacity-90 tracking-wider"
+          onClick={handleSignOut}
+        >
           Sign out
         </span>
       </div>
       <button
-          className="bg-green-700 text-white p-3 mt-2 rounded-lg hover:opacity-95 disabled:opacity-80 w-full"
-          style={{ fontFamily: "sans-serif" }}
+          className="bg-green-700 text-white p-3 mt-2 rounded-lg hover:opacity-95 disabled:opacity-80 w-full tracking-wider"
           // onClick={handleSubmit}
           // disabled={loading}
         >
           Create Listing
         </button>
       <button
-          className="bg-green-700 text-white p-3 mt-2 rounded-lg hover:opacity-95 disabled:opacity-80 w-full"
-          style={{ fontFamily: "sans-serif" }}
+          className="bg-green-700 text-white p-3 mt-2 rounded-lg hover:opacity-95 disabled:opacity-80 w-full tracking-wider"
           // onClick={handleSubmit}
           // disabled={loading}
         >
