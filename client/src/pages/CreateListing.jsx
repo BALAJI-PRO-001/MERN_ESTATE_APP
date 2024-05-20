7dd0a6ce7893aaac6c9a6ac8d872ad0746391a1a
BALAJI-PRO-001 <tempEmailForCoding000000@gmail.com>
2024-05-20 15:03:20 +0530

    Feat: complete create listing page UI

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,8 +1,8 @@
 export default function CreateListing() {
   return (
     <main className="p-3 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7 text-gray-700">Create a Listing</h1>
-      <form className="flex flex-col sm:flex-row">
+      <h1 className="text-3xl font-semibold text-center my-7">Create a Listing</h1>
+      <form className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
           <input
             id="name"
@@ -56,7 +56,7 @@ export default function CreateListing() {
               <input 
                 type="number" 
                 id="bedrooms" 
-                min="1" max="10" required size="8"
+                min="1" max="10" required 
                 className="p-3 border rounded-lg focus:outline-none focus:border-green-600"
               />
               <p>Beds</p>
@@ -65,7 +65,7 @@ export default function CreateListing() {
               <input 
                 type="number" 
                 id="bathrooms" 
-                min="1" max="10" required size="8"
+                min="1" max="10" required 
                 className="p-3 border rounded-lg focus:outline-none focus:border-green-600"
               />
               <p>Bathrooms</p>
@@ -74,25 +74,52 @@ export default function CreateListing() {
               <input 
                 type="number" 
                 id="regular-price" 
-                min="50" required size="8"
+                min="50" required 
                 className="p-3 border rounded-lg focus:outline-none focus:border-green-600"
               />
-              <p>Regular price</p> 
-              <span className="text-xs">($ / month)</span>
+              <div className="flex flex-col text-center">
+                <p>Regular price</p> 
+                <span className="text-xs">($ / month)</span>
+              </div>
             </div>
             <div className="flex items-center gap-1">
               <input 
                 type="number" 
                 id="discounted-price" 
-                min="50" required size="8"
+                min="50" required 
                 className="p-3 border rounded-lg focus:outline-none focus:border-green-600"
               />
-              <p>Discounted price</p>
-              <span className="text-xs">($ / month)</span>
+              <div className="flex flex-col text-center">
+                <p>Discounted price</p> 
+                <span className="text-xs">($ / month)</span>
+              </div>
             </div>
           </div>
 
         </div>
+
+        <div className="flex flex-col flex-1 gap-3">
+          <p className="font-semibold">Images: 
+            <span className="font-normal text-gray-600 pl-2"> The first image will be the cover (max 6)</span>
+          </p>
+
+          <div className="flex gap-3">
+            <input 
+              type="file" 
+              id="images" 
+              accept="image/*" 
+              multiple 
+              className="p-3 rounded w-full border border-gray-500"
+            />
+            <button 
+              className="uppercase rounded-lg p-3 bg-green-600 text-white font-semibold hover:shadow-lg hover:opacity-85 disabled:opacity-70 tracking-wider" 
+            >
+              Upload
+            </button>
+          </div>
+          <button className="p-3 bg-slate-700 rounded-lg text-white tracking-wider font-semibold uppercase hover:opacity-85">Create Listing</button>
+        </div>
+
       </form>
     </main>
   );
