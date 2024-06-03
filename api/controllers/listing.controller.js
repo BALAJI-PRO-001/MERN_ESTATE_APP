f18cc6be5a58d035a5aa4ca579e4b08b27b3e139
BALAJI-PRO-001 <emailForGIT@gmail.com>
2024-06-03 16:31:24 +0530

    Feat: delete functionality added

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import errorHandler from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -8,6 +9,25 @@ export const createListing = async (req, res, next) => {
       listing: listing
     });
   } catch (error) {
+    next(error);
+  }
+}
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
+  if (req.verifiedUserId !== listing.userRef) {
+    return next(errorHandler(404, "Unauthorized"));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(204).json({});
+  } catch(error) {
     next(error);
   }
 }
