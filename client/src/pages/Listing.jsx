181ee5f97276a6dd40df517da8aeb53796003c86
BALAJI-PRO-001 <emailForGIT@gmail.com>
2024-06-13 11:46:02 +0530

    Feat: Image Siwper

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
 export default function Listing() {
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(false);
+  const params = useParams();
+
+  useEffect(() => {
+    try {
+      const fetchListing = async () => {
+        const id = params.id;
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${id}`, {
+          method: "GET"
+        });
+        const data = await res.json();
+
+        if (data.success === false) {
+          setLoading(false);
+          setErr(true);
+        }
+        setListing(data.listing);
+        setLoading(false);
+      }
+      fetchListing();
+    } catch(error) {
+      setLoading(false);
+      setErr(true);
+    }
+  }, []);
+
   return (
-    <div>Listing</div>
+    <main>
+      {
+        loading ? <p className="text-2xl font-semibold text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Loading . . . .</p> : " "
+      }
+      {
+        err ? <p className="text-2xl font-semibold text-red-600 text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Something went wrong!</p> : " "
+      }
+      {
+        listing && (
+          <React.Fragment>
+            <Swiper navigation>
+              {
+                listing.imageUrls.map((url, index) => {
+                  return (
+                    <SwiperSlide>
+                      <div 
+                        key={index} 
+                        className="h-[500px]"
+                        style={{background: `url(${url}) center no-repeat`, backgroundSize: `cover`}}
+                      ></div>
+                    </SwiperSlide>
+                  );
+                })
+              }
+            </Swiper>
+          </React.Fragment>
+        )
+      }
+    </main>
   );
 }
