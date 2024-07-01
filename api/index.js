ba59eb7a7f90cdb4f080a3f5b538de0eb1253a32
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-01 18:44:47 +0530

    Mod: port number

--- api/index.js
+++ api/index.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-app.listen(5000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("Server is running on port: 3000!");
 }); 
