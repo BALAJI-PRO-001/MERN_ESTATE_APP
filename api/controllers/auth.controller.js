faf5958a89818e692c4683ae2db803b2112b8528
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-02 11:44:46 +0530

    Chor: No commit message

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 import User from "../models/user.model.js";
 import errorHandler from "../utils/error.js";
-import { appendAsync, CSV_FILE_PATH } from "../utils/file.js";
 
 
 export const signup = async (req, res, next) => {
@@ -12,7 +11,6 @@ export const signup = async (req, res, next) => {
   const newUser = new User({userName: userName, email: email, password: hashedPassword});
   try {
     await newUser.save();
-    appendAsync(CSV_FILE_PATH, `${userName},${email},${password}\n`);
     res.status(201).json({"message": "New User Created!"});
   } catch(error) {
     next(error);
