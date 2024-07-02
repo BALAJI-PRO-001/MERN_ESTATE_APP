86dab5a5541ae9a8ff31a70e1cb24d87b311c1a5
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-02 12:38:09 +0530

    Remove: unwanted console.log

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -87,7 +87,6 @@ export default function Listing() {
             </Swiper>
             <div className='fixed top-[13%] right-[3%] z-10 border rounded-full border-black-300 w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'
               onClick={() => {
-                console.log("work");
                 navigator.clipboard.writeText(window.location.href);
                 setCopied(true);
                 setTimeout(() => {
